7351a18ffe89962631429755e67bb67dbb66dff0
Albaebauluz Alba <albaebauluz@hotmail.com>
2024-08-18 11:06:03 +0000

    Modelo

--- src/app.py
+++ src/app.py
@@ -2,3 +2,22 @@ from utils import db_connect
 engine = db_connect()
 
 # your code here
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import missingno as msno
+from sklearn.impute import KNNImputer
+import statsmodels.api as sm
+from sklearn.model_selection import train_test_split
+import xgboost as xgb
+from statsmodels.stats.outliers_influence import variance_inflation_factor
+from sklearn.preprocessing import MinMaxScaler
+from lazypredict.Supervised import LazyRegressor
+from sklearn.ensemble import ExtraTreesRegressor
+from sklearn.model_selection import RandomizedSearchCV
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import mean_absolute_error, mean_absolute_percentage_error, r2_score
+from sklearn.ensemble import BaggingRegressor
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import FunctionTransformer
+import joblib
